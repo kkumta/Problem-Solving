3af90f37cc77b5e7f19fd85a2cfad7ff973b8903
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-02-06 22:33:09 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,55 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int N, K;
+int times[100001];
+
+void bfs()
+{
+	queue<int> q;
+	q.push(N);
+	while (!q.empty())
+	{
+		int curPos = q.front(); q.pop();
+		{
+			int nextPos = curPos + 1;
+			if (nextPos < 0 || nextPos > 100000);
+			else if (times[nextPos] == 0 || times[nextPos] > times[curPos] + 1)
+			{
+				times[nextPos] = times[curPos] + 1;
+				q.push(nextPos);
+			}
+		}
+		{
+			int nextPos = curPos - 1;
+			if (nextPos < 0 || nextPos > 100000);
+			else if (times[nextPos] == 0 || times[nextPos] > times[curPos] + 1)
+			{
+				times[nextPos] = times[curPos] + 1;
+				q.push(nextPos);
+			}
+		}
+		{
+			int nextPos = curPos * 2;
+			if (nextPos < 0 || nextPos > 100000);
+			else if (times[nextPos] == 0 || times[nextPos] > times[curPos] + 1)
+			{
+				times[nextPos] = times[curPos] + 1;
+				q.push(nextPos);
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> N >> K;
+
+	times[N] = 1;
+	bfs();
+
+	cout << times[K] - 1;
+}
