05bae56432672d3fdfe019c62cc1fe064798ecf5
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-02-09 01:44:57 +0900

    [Gold IV] Title: 빙산, Time: 100 ms, Memory: 2468 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -10,8 +10,8 @@ short dx[]{ 1, 0, -1, 0 };
 short dy[]{ 0, 1, 0, -1 };
 
 int N, M, year;
-short curIceberg[301][301];
-short nextIceberg[301][301];
+short iceberg[301][301];
+short zero[301][301];
 bool visited[301][301];
 
 void CheckIceberg(pair<int, int> startPos)
@@ -26,7 +26,7 @@ void CheckIceberg(pair<int, int> startPos)
 		{
 			pair<int, int> nextPos = make_pair(curPos.row + dy[i], curPos.col + dx[i]);
 			if (N <= nextPos.row || nextPos.row < 0 || M <= nextPos.col || nextPos.col < 0) continue;
-			if (curIceberg[nextPos.row][nextPos.col] == 0) continue;
+			if (iceberg[nextPos.row][nextPos.col] == 0) continue;
 			if (visited[nextPos.row][nextPos.col]) continue;
 			q.push(nextPos);
 			visited[nextPos.row][nextPos.col] = true;
@@ -34,14 +34,35 @@ void CheckIceberg(pair<int, int> startPos)
 	}
 }
 
-void MeltingIceberg(pair<int, int> waterPos)
+void CheckZeroCount(pair<int, int> icebergPos)
 {
 	for (int i = 0; i < 4; i++)
 	{
-		pair<int, int> icebergPos = make_pair(waterPos.row + dy[i], waterPos.col + dx[i]);
-		if (N <= icebergPos.row || icebergPos.row < 0 || M <= icebergPos.col || icebergPos.col < 0) continue;
-		if (nextIceberg[icebergPos.row][icebergPos.col] == 0) continue;
-		nextIceberg[icebergPos.row][icebergPos.col]--;
+		pair<int, int> waterPos = make_pair(icebergPos.row + dy[i], icebergPos.col + dx[i]);
+		if (N <= waterPos.row || waterPos.row < 0 || M <= waterPos.col || waterPos.col < 0) continue;
+		if (iceberg[waterPos.row][waterPos.col] == 0)
+			zero[icebergPos.row][icebergPos.col]++;
+	}
+}
+
+void MeltingIceberg()
+{
+	for (int i = 1; i < N - 1; i++)
+	{
+		for (int j = 1; j < M - 1; j++)
+		{
+			if (iceberg[i][j] != 0)
+				CheckZeroCount(make_pair(i, j));
+		}
+	}
+
+	for (int i = 1; i < N - 1; i++)
+	{
+		for (int j = 1; j < M - 1; j++)
+		{
+			if (iceberg[i][j] != 0 && zero[i][j] > 0)
+				iceberg[i][j] = iceberg[i][j] - zero[i][j] > 0 ? iceberg[i][j] - zero[i][j] : 0;
+		}
 	}
 }
 
@@ -54,21 +75,19 @@ int main()
 	cin >> N >> M;
 	for (int i = 0; i < N; i++)
 		for (int j = 0; j < M; j++)
-			cin >> curIceberg[i][j];
+			cin >> iceberg[i][j];
 
 	while (true)
 	{
 		// BFS로 빙산이 두 덩어리 이상으로 분리되는지 확인
-		memset(visited, false, sizeof(visited));
 		int icebergCnt = 0;
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < M; j++)
-				if (curIceberg[i][j] != 0 && visited[i][j] == false)
+				if (iceberg[i][j] != 0 && visited[i][j] == false)
 				{
 					icebergCnt++;
 					CheckIceberg(make_pair(i, j));
 				}
-
 		if (icebergCnt == 0)
 		{
 			cout << 0;
@@ -76,23 +95,12 @@ int main()
 		}
 		else if (icebergCnt > 1) break;
 
-		// 새로운 빙산 배열에 원래 빙산 복사
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < M; j++)
-				nextIceberg[i][j] = curIceberg[i][j];
-
-		// 완전탐색으로 빙산 녹이기
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < M; j++)
-				if (curIceberg[i][j] == 0)
-					MeltingIceberg(make_pair(i, j));
-
-		// 원래 빙산 배열에 새로운 빙산 복사
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < M; j++)
-				curIceberg[i][j] = nextIceberg[i][j];
+		// 완전탐색으로 빙산 주변의 바닷물 개수 구해서 빙산 녹이기
+		MeltingIceberg();
 
 		year++;
+		memset(visited, false, sizeof(visited));
+		memset(zero, 0, sizeof(zero));
 	}
 
 	cout << year;
