a304656ecf123a03dda8d0d77e02ee844eae36ca
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-02-11 20:06:54 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 464 ms, Memory: 47904 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.cc
@@ -0,0 +1,78 @@
+#include <tuple>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+int dy[]{ 1, 0, -1, 0 };
+int dx[]{ 0, 1, 0, -1 };
+
+int N, M, K;
+char map[1001][1001];
+int dist[11][1001][1001];
+
+void BFS()
+{
+	queue<tuple<int, int, int>> q;
+	q.push(make_tuple(0, 0, 0));
+	dist[0][0][0] = 1;
+	while (!q.empty())
+	{
+		tuple<int, int, int> curPos = q.front(); q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			int curBreakCount = get<0>(curPos);
+			int nextRow = get<1>(curPos) + dy[i];
+			int nextCol = get<2>(curPos) + dx[i];
+
+			if (N <= nextRow || nextRow < 0 || M <= nextCol || nextCol < 0) continue;
+			if (curBreakCount < K)
+			{
+				// 부수지 않고 이동
+				if (map[nextRow][nextCol] == '0' && dist[curBreakCount][nextRow][nextCol] == 0)
+				{
+					q.push(make_tuple(curBreakCount, nextRow, nextCol));
+					dist[curBreakCount][nextRow][nextCol] = dist[curBreakCount][get<1>(curPos)][get<2>(curPos)] + 1;
+				}
+				// 부수고 이동
+				if (map[nextRow][nextCol] == '1' && dist[curBreakCount + 1][nextRow][nextCol] == 0)
+				{
+					q.push(make_tuple(curBreakCount + 1, nextRow, nextCol));
+					dist[curBreakCount + 1][nextRow][nextCol] = dist[curBreakCount][get<1>(curPos)][get<2>(curPos)] + 1;
+				}
+			}
+			else
+			{
+				// 부수지 않고 이동
+				if (map[nextRow][nextCol] == '0' && dist[curBreakCount][nextRow][nextCol] == 0)
+				{
+					q.push(make_tuple(curBreakCount, nextRow, nextCol));
+					dist[curBreakCount][nextRow][nextCol] = dist[curBreakCount][get<1>(curPos)][get<2>(curPos)] + 1;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N >> M >> K;
+	for (int i = 0; i < N; i++)
+	{
+		string temp;
+		cin >> temp;
+		for (int j = 0; j < M; j++)
+			map[i][j] = temp[j];
+	}
+
+	BFS();
+
+	int ans = 1000001;
+	for (int i = 0; i <= K; i++)
+		if (dist[i][N - 1][M - 1] != 0 && dist[i][N - 1][M - 1] < ans)
+			ans = dist[i][N - 1][M - 1];
+	if (ans == 1000001) cout << -1;
+	else cout << ans;
+}
