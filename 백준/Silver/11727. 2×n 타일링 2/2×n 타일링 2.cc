612cd405346c753d2ffb123aad6726dfa46edeb9
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-04-03 23:47:58 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int n;
+int d[1001];
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n;
+
+	d[1] = 1;
+	d[2] = 3;
+
+	for (int i = 3; i <= n; i++)
+		d[i] = (d[i - 2] * 2 + d[i - 1]) % 10007;
+
+	cout << d[n];
+}
