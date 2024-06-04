b8f034c52f30c85e265b643023ab82726773217a
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-06-04 21:11:07 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -1,61 +1,43 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int N;
-int picture[65][65];
+int maps[64][64];
 
-void func(pair<int, int> startPos, int n)
-{
-	if (n == 1)
-	{
-		cout << picture[startPos.first][startPos.second];
-		return;
-	}
-
-	int cnt0 = 0, cnt1 = 0;
-	for (int i = startPos.first; i < startPos.first + n; i++)
-		for (int j = startPos.second; j < startPos.second + n; j++)
+string go(int y, int x, int n)
+{	
+	// 모두 같은 숫자로만 되어 있을 경우
+	int cnt[] = {0, 0}; 
+	for (int i = y; i < y + n; i++)
+		for (int j = x; j < x + n; j++)
 		{
-			if (picture[i][j] == 0)
-				cnt0++;
-			else
-				cnt1++;
+			if (maps[i][j] == 0) cnt[0]++;
+			else cnt[1]++;
 		}
-	if (cnt0 == 0) // 모든 자리 숫자가 1이면
-	{
-		cout << '1';
-		return;
-	}
-	else if (cnt1 == 0) // 모든 자리 숫자가 0이면
-	{
-		cout << '0';
-		return;
-	}
-	else
-	{
-		n /= 2;
-		cout << '(';
-		func(startPos, n);
-		func(make_pair(startPos.first, startPos.second + n), n);
-		func(make_pair(startPos.first + n, startPos.second), n);
-		func(make_pair(startPos.first + n, startPos.second + n), n);
-		cout << ')';
-	}
+	if (cnt[0] == 0) return "1";
+	else if (cnt[1] == 0) return "0";
+	
+	// 모두 같은 숫자로만 되어 있지 않을 경우 
+	string ret = "";
+	ret += '(';
+	ret += go(y, x, n / 2);
+	ret += go(y, x + n / 2, n / 2);
+	ret += go(y + n / 2, x, n / 2);
+	ret += go(y + n / 2, x + n / 2, n / 2);
+	ret += ')';
+	return ret;
 }
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
-		string temp;
-		cin >> temp;
+		string s;
+		cin >> s;
 		for (int j = 0; j < N; j++)
-			picture[i][j] = temp[j] - '0';
+			maps[i][j] = s[j] - '0';
 	}
-
-	func(make_pair(0, 0), N);
+	
+	cout << go(0, 0, N);
 }
