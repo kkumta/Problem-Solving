fe4d5a521b70cf5347aaeff39531b9f78570a52b
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-07-25 13:59:40 +0900

    [Silver V] Title: 피카츄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14405. 피카츄/피카츄.cc
+++ 백준/Silver/14405. 피카츄/피카츄.cc
@@ -2,40 +2,24 @@
 using namespace std;
 
 string S;
-int _size;
 bool ret = true;
 
 int main()
 {
 	cin >> S;
-	_size = S.size();
-	for (int i = 0; i < S.size() - 1; i++)
+	for (int i = 0; i < S.size(); i++)
 	{
-		if (S[i] == 'p' && S[i + 1] == 'i')
-		{
+		if (i < S.size() - 1 && ((S[i] == 'p' && S[i + 1] == 'i') || S[i] == 'k' && S[i + 1] == 'a'))
 			i += 1;
-			_size -= 2;
-		}
-		else if (S[i] == 'k' && S[i + 1] == 'a')
-		{
-			i += 1;
-			_size -= 2;
-		}
 		else if (i < S.size() - 2 && S[i] == 'c' && S[i + 1] == 'h' && S[i + 2] == 'u')
-		{
 			i += 2;
-			_size -= 3;
-		}
 		else
 		{
 			ret = false;
 			break;
 		}
 	}
-	
-	if (_size > 0)
-		ret = false;
-	
+
 	if (ret)
 		cout << "YES";
 	else
