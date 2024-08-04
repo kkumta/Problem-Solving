87d57f3d268e0b7727a327978538e79311cfc7a2
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-08-04 23:29:13 +0900

    [Bronze III] Title: 세 막대, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[3];
+
+int main()
+{
+	cin >> a[0] >> a[1] >> a[2];
+	sort(&a[0], &a[0] + 3);
+	
+	if (a[0] + a[1] > a[2]) cout << a[0] + a[1] + a[2];
+	else cout << (a[0] + a[1]) * 2 - 1;
+}
