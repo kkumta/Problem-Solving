b2edb3cc5cc95f4061b12fe81f5da439485de2d9
Choi Dasom <54059732+kkumta@users.noreply.github.com>
2024-08-05 23:01:10 +0900

    [Gold IV] Title: 문자열 폭발, Time: 48 ms, Memory: 8756 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -1,50 +1,40 @@
-#include <stack>
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-stack<pair<char, int>> stk, ans;
-string str1, str2;
+string a, b, ret;
+stack<char> stk;
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> str1 >> str2;
-	for (int i = 0; i < str1.size(); i++)
+	cin >> a >> b;
+	for (int i = 0; i < a.size(); i++)
 	{
-		if (!stk.empty() && str1[i] == str2[stk.top().second]) // 폭발 문자열의 문자가 연속된다
+		stk.push(a[i]);
+		if (stk.size() >= b.size() && stk.top() == b[b.size() - 1])
 		{
-			// 새로운 문자도 스택에 넣어 준다
-			stk.push(make_pair(str1[i], stk.top().second + 1));
-			if (stk.top().second == str2.size()) // 폭발!
+			string s = "";
+			for (int j = 0; j < b.size(); j++)
 			{
-				for (int j = 0; j < str2.size(); j++)
-					stk.pop();
-			}
-		}
-		else if (str1[i] == str2[0]) // 폭발 문자열의 첫번째 문자가 str1[i]이다
-		{
-			stk.push(make_pair(str1[i], 1)); // 새로운 문자를 스택에 넣어 준다
-			if (str2.size() == 1) // 폭발!
+				s += stk.top();
 				stk.pop();
+			}
+			reverse(s.begin(), s.end());
+			
+			if (s != b)
+				for (char c : s)
+					stk.push(c);
 		}
-		else // str1[i]가 폭발 문자열과 관련이 없다
-			stk.push(make_pair(str1[i], 0));
 	}
-
-	if (stk.size() == 0) cout << "FRULA";
+	
+	if (stk.empty()) cout << "FRULA";
 	else
 	{
 		while (!stk.empty())
 		{
-			ans.push(stk.top());
+			ret += stk.top();
 			stk.pop();
 		}
-		while (!ans.empty())
-		{
-			cout << ans.top().first;
-			ans.pop();
-		}
+		reverse(ret.begin(), ret.end());
+		cout << ret;
 	}
 }
